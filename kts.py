5515da0b961ee746b45cda2f5b554fb28b5f3d4e
m4rtin <martin.veselovskyy@gmail.com>
2015-05-10 17:33:17 +0200

    Numbthy added for modular functions.
    Working quite well

--- kts.py
+++ kts.py
@@ -1,29 +1,47 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'Martin Veselovsky'
+# Author: Bc. Martin Veselovsky
+# Email:  xvesel60@stud.fit.vutbr.cz
 
-import logging
-
-logger = logging.getLogger(__name__)
+from numbthy import is_prime, is_primitive_root, power_mod, factor
 
 
 class KTS():
+    """
+    Kirkman Triple System (KTS)
+
+    Implementation based on publication:
+
+        Stinson, D.: A survey of Kirkman triple systems and related designs.
+        Discrete Mathematics, Volume 92, Issues 1–3, 17 November 1991, Pages 371–393,
+        ISSN 0012-365X, doi:http://dx.doi.org/10.1016/0012-365X(91)90294-C.
+        URL http://www.sciencedirect.com/science/article/pii/0012365X9190294C
+    """
+
     def __init__(self, order):
+        """
+        Initialize with proper construction method.
+        Raise ValueError if KTS order number does not satisfy necessary condition
+        for creation of Kirkman Triple System.
+        :param order: order of KTS, e.g. number of participants
+        """
         if order % 6 != 3:
             raise ValueError('Order number %s does not satisfy necessary '
                              'condition "order `mod` 6 = 3"'.format(order))
 
         # Construction 1.1 [45, Theorem 6]
         self.q = int(order / 2)
-        if order % 2 == 1 and self.isprime(self.q):
+        if order % 2 == 1 and self.is_prime_power(self.q) and self.q % 6 == 1:
+            self.method_name = 'Construction 1.1'
             self.create_blocks = self.create_blocks_1
             self.create_parallel = self.create_parallel_1
 
         # Construction 1.2 [45, Theorem 5]
         else:
             self.q = int(order / 3)
-            if order % 3 == 0 and self.isprime(self.q):
+            if order % 3 == 0 and self.is_prime_power(self.q) and self.q % 6 == 1:
+                self.method_name = 'Construction 1.2'
                 self.create_blocks = self.create_blocks_2
                 self.create_parallel = self.create_parallel_2
             else:
@@ -35,6 +53,28 @@ class KTS():
         self.points = range(1, order + 1)
         self.blocks = {}
         self.classes = {}
+        self.solution = {}
+
+    @staticmethod
+    def is_prime_power(n):
+        if is_prime(n):
+            return True
+        if len(factor(n)) == 1:
+            return True
+        return False
+
+    def solve(self):
+        """
+        Solve problem by construction 1.1 or 1.2 depending on KTS order.
+        :return: dictionary of days containing array of triples for each day
+        """
+        self.create_parallel()
+        day = 0
+        for class_blocks in self.classes.values():
+            day += 1
+            for block_triple in class_blocks.values():
+                self.solution.setdefault(day, []).append(block_triple)
+        return self.solution
 
     def create_parallel_1(self):
         """
@@ -42,80 +82,83 @@ class KTS():
         Obtain parallel classes by developing through Galois Field (self.q)
         """
         for i in range(self.q):
-            self.create_blocks(number=i)
+            self.create_blocks(i)
             self.classes[i] = self.blocks
 
-    def create_blocks_1(self, alpha=3, number=0):
+    def create_blocks_1(self, number=0):
         """
         Construction 1.1
         Obtain blocks that create one parallel class.
-        :param alpha: primitive element in Galois Field (self.q)
         :param number: iteration (offset) number according to Galois Field
         """
-        m = 0
 
-        right = self.add_mod(self.pow_mod(alpha, self.t), 1)
-        while m < 10:
-            alpha_m = self.pow_mod(alpha, m)
-            left = self.add_mod(alpha_m, alpha_m)
+        # first obtain alpha (primitive element in Galois Field) and m (by specified equation)
+        alpha, m = 0, 0
+        for a in range(self.q):
+            if is_primitive_root(a, self.q):
+                for _m in range(self.q):
+                    # let me satisfy the equation --> 2 * alpha^m = alpha^t + 1
+                    right = self.add_mod(power_mod(a, self.t, self.q), 1)
+                    _a = power_mod(a, _m, self.q)
+                    left = self.add_mod(_a, _a)
+                    if left == right:
+                        alpha, m = a, _m
+                        break
 
-            print('right: %s, left: %s, m: %s' % (right, left, m))
+        # split points to groups
+        groups = {
+            1: self.points[:self.q],
+            2: self.points[self.q:],
+        }
 
-            if left == right:
-                break
-            m += 1
-        print('alpha = %s, m = %s' % (alpha, m))
-
-        get = lambda index: self.points[index]
-        get1 = lambda index: get(self.add_mod(index, number))
-        get2 = lambda index: get(self.add_mod(index, number) + self.q)
+        get = lambda index, group: groups[group][self.add_mod(index, number)]
 
         # references A^0
-        self.blocks = {'a_': (self.points[number],
-                              self.points[number + self.q],
-                              self.points[-1])}
+        self.blocks = {'a_': (groups[1][number],
+                              groups[2][number],
+                              groups[2][-1])}
 
         # references A_i, B_i
         for i in range(0, self.t):
             self.blocks['a' + str(i)] = \
-                (get2(self.pow_mod(alpha, i + m + self.t)),
-                 get2(self.pow_mod(alpha, i + m + 3 * self.t)),
-                 get2(self.pow_mod(alpha, i + m + 5 * self.t)))
+                (get(power_mod(alpha, i + m + self.t, self.q), 2),
+                 get(power_mod(alpha, i + m + 3 * self.t, self.q), 2),
+                 get(power_mod(alpha, i + m + 5 * self.t, self.q), 2))
 
             self.blocks['b' + str(i)] = \
-                (get1(self.pow_mod(alpha, i)),
-                 get1(self.pow_mod(alpha, i + self.t)),
-                 get2(self.pow_mod(alpha, i + m)))
+                (get(power_mod(alpha, i, self.q), 1),
+                 get(power_mod(alpha, i + self.t, self.q), 1),
+                 get(power_mod(alpha, i + m, self.q), 2))
 
         # references B_i
         for i in range(2 * self.t, 3 * self.t):
             self.blocks['b' + str(i)] = \
-                (get1(self.pow_mod(alpha, i)),
-                 get1(self.pow_mod(alpha, i + self.t)),
-                 get2(self.pow_mod(alpha, i + m)))
+                (get(power_mod(alpha, i, self.q), 1),
+                 get(power_mod(alpha, i + self.t, self.q), 1),
+                 get(power_mod(alpha, i + m, self.q), 2))
 
         # references B_i
         for i in range(4 * self.t, 5 * self.t):
             self.blocks['b' + str(i)] = \
-                (get1(self.pow_mod(alpha, i)),
-                 get1(self.pow_mod(alpha, i + self.t)),
-                 get2(self.pow_mod(alpha, i + m)))
+                (get(power_mod(alpha, i, self.q), 1),
+                 get(power_mod(alpha, i + self.t, self.q), 1),
+                 get(power_mod(alpha, i + m, self.q), 2))
 
     def create_parallel_2(self):
         """
         Construction 1.2
         Obtain parallel classes by developing through Galois Field (self.q).
-        Another parallel classes are made from 'a_i' blocks. These are referenced as remainder
+        Other parallel classes are made from 'a_i' blocks. These are referenced as remainder
         triples which means, that these triples are arranged by 'block_key' with values
-        constructed as (origin class_name, triple).
+        constructed as (origin class name, triple).
         """
 
         # first obtain all parallel classes developing through Galois Field
         # but set of parallel classes are super set of real parallel classes at the moment
         self.create_parallel_1()
 
-        real_classes = {}           # i.e.   0  { 'a_': (1, 8, 15) ,  'b0,1': (2, 3, 5), ... }
-        remainder_triples = {}      # i.e.   a2 {    0: (3, 12, 16),       1: (4, 13, 17), ... }
+        real_classes = {}       # i.e.   0  { 'a_': (1, 8, 15) ,  'b0,1': (2, 3, 5), ... }
+        remainder_triples = {}  # i.e.   a2 {    0: (3, 12, 16),       1: (4, 13, 17), ... }
 
         # split super set to get real parallel classes and remainder triples
         for class_key, class_blocks in self.classes.items():
@@ -132,7 +175,6 @@ class KTS():
                             or 5 * self.t <= int(i) <= 6 * self.t - 1:
                         real_classes[class_key][block_key] = block_triple
                     else:
-                        # swap block_key/class_key
                         remainder_triples.setdefault(block_key, {})[class_key] = block_triple
 
                 # 'b_i' blocks logic
@@ -140,68 +182,27 @@ class KTS():
                     if 0 <= int(i) <= self.t - 1:
                         real_classes[class_key][block_key] = block_triple
                     else:
-                        # remainder_triples[class_key][block_key] = block_triple
                         remainder_triples.setdefault(block_key, {})[class_key] = block_triple
 
-        for k, v in real_classes.items():
-            print(k, v)
-
-        for k, v in remainder_triples.items():
-            print(k, v)
-
-        # another = {}
-        # for class_key, class_blocks in remainder_triples.items():
-        #     for block_key, block_triple in class_blocks.items():
-        #         another.setdefault(block_key, []).append(block_triple)
-        #
-        # for k, v in another.items():
-        #     print(k, v)
-        #
-        # self.classes = {}
-        # for class_key, class_blocks in real_classes.items():
-        #     for block_triple in class_blocks.values():
-        #         self.classes.setdefault(class_key, []).append(block_triple)
-        # for class_key, class_blocks in another.items():
-        #     for block_triple in class_blocks:
-        #         self.classes.setdefault(class_key, []).append(block_triple)
-
         self.classes = {}
-        for class_key, class_blocks in real_classes.items():
-            for block_triple in class_blocks.values():
-                self.classes.setdefault(class_key, []).append(block_triple)
-        for class_key, class_blocks in remainder_triples.items():
-            for block_triple in class_blocks.values():
-                self.classes.setdefault(class_key, []).append(block_triple)
+        self.classes.update(real_classes)
+        self.classes.update(remainder_triples)
 
-        self.print_classes()
-        print()
-
-    def am(self, number, addition):
-        result = number + addition
-        if number < self.q:
-            if result > self.q:
-                return result - self.q
-            else:
-                return result
-        elif self.q <= number < self.q * 2:
-            if result > self.q * 2:
-                return result - self.q
-            else:
-                return result
-        elif number >= self.q * 2:
-            if result > self.q * 3:
-                return result - self.q
-            else:
-                return result
-
-    def create_blocks_2(self, alpha=3, number=0):
+    def create_blocks_2(self, number=0):
         """
         Construction 1.2
         Obtain blocks that create super set of one parallel class.
-        :param alpha: primitive element in Galois Field (self.q)
         :param number: iteration (offset) number according to Galois Field
         """
 
+        # first obtain alpha (primitive element in Galois Field)
+        alpha = 0
+        for a in range(self.q):
+            if is_primitive_root(a, self.q):
+                alpha = a
+                break
+
+        # split points to groups
         groups = {
             1: self.points[:self.q],
             2: self.points[self.q:self.q * 2],
@@ -217,84 +218,54 @@ class KTS():
         for i in range(0, self.t):
             for j in (1, 2, 3):
                 self.blocks['b' + str(i) + ',' + str(j)] = \
-                    (get(self.pow_mod(alpha, i), j),
-                     get(self.pow_mod(alpha, i + 2 * self.t), j),
-                     get(self.pow_mod(alpha, i + 4 * self.t), j))
+                    (get(power_mod(alpha, i, self.q), j),
+                     get(power_mod(alpha, i + 2 * self.t, self.q), j),
+                     get(power_mod(alpha, i + 4 * self.t, self.q), j))
 
         # references A_i
         for i in range(0, 6 * self.t):
             self.blocks['a' + str(i)] = \
-                (get(self.pow_mod(alpha, i), 1),
-                 get(self.pow_mod(alpha, i + 2 * self.t), 2),
-                 get(self.pow_mod(alpha, i + 4 * self.t), 3))
+                (get(power_mod(alpha, i, self.q), 1),
+                 get(power_mod(alpha, i + 2 * self.t, self.q), 2),
+                 get(power_mod(alpha, i + 4 * self.t, self.q), 3))
 
-            # parallel = {}
-            # for k, v in self.blocks.items():
-            # if k[0] == 'a':
-            #         if k[1] == '_':
-            #             parallel[k] = v
-            #         else:
-            #             i = int(k[1])
-            #             if self.t <= i <= 2 * self.t - 1 \
-            #                     or 3 * self.t <= i <= 4 * self.t - 1 \
-            #                     or 5 * self.t <= i <= 6 * self.t - 1:
-            #                 parallel[k] = v
-            #     elif k[0] == 'b':
-            #         i = int(k[1])
-            #         if 0 <= i <= self.t - 1:
-            #             parallel[k] = v
-            # self.blocks = parallel
+    def test_classes(self):
+        """
+        Test all parallel classes blocks for uniqueness and completeness within the class.
+        """
+        triples = set()
+        for c in self.classes.values():
+            row = set()
+            for b in c.values():
+                triples.add(b)
+                [row.add(x) for x in b]
 
-    def pow_mod(self, number, power):
-        return (number ** power) % self.q
+            assert len(row) == self.order, \
+                'There are missing %s / %s values in class' % (len(row), self.order)
+        assert len(triples) == len(self.classes) * len(self.classes[0]), \
+            'There are some triples more times than one'
 
     def add_mod(self, number, addition):
         result = number + addition
         return result if result < self.q else result % self.q
 
-    def __str__(self):
-        s = 'Points: %s \n' % str([p for p in self.points])
-        s += 'Prime power q: %s \n' % self.q
-        s += 'Blocks: \n'
-        for k, v in self.blocks.items():
-            s += k + ': ' + str(v) + '\n'
-        return s
-
     def print_classes(self):
         for k, v in self.classes.items():
             print(k, v)
 
-    def test_classes(self):
-        triples = set()
-        for c in self.classes.values():
-            row = set()
-            for b in c:     # ????? .values()
-                triples.add(b)
-                [row.add(x) for x in b]
+    def print_solution(self):
+        for day, blocks in self.solution.items():
+            print('Day %2s: ' % day, end='')
+            for block_triple in blocks:
+                print(str(block_triple).ljust(12), end='  ')
+            print()
 
-            assert len(row) == self.order
-        assert len(triples) == len(self.classes) * len(self.classes[0])
-
-    # method taken from
-    # https://www.daniweb.com/software-development/python/
-    # code/216880/check-if-a-number-is-a-prime-number-python
-    @staticmethod
-    def isprime(n):
-        """check if integer n is a prime"""
-        # make sure n is a positive integer
-        n = abs(int(n))
-        # 0 and 1 are not primes
-        if n < 2:
-            return False
-        # 2 is the only even prime number
-        if n == 2:
-            return True
-        # all other even numbers are not primes
-        if not n & 1:
-            return False
-        # range starts with 3 and only needs to go up the squareroot of n
-        # for all odd numbers
-        for x in range(3, int(n ** 0.5) + 1, 2):
-            if n % x == 0:
-                return False
-        return True
+    def __str__(self):
+        s = 'Kirkman triple system (KTS)\n' \
+            '  Order: %s \n' \
+            '  Points %s \n' \
+            '  Prime power: %s \n' \
+            '  Method used: %s \n' \
+            '  Solution: %s days' % \
+            (self.order, self.points, self.q, self.method_name, len(self.solution))
+        return s
